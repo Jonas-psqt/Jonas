75631f874e8c4cf652fa11729eabbf457fdbfef5
Jonas-psqt <jonas.pasquet@hotmail.fr>
2019-10-01 20:41:33 +0200

    test

---
+++ exo_07_a.rb
@@ -0,0 +1,4 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+#gets est une commande qui retranscrit ce que l'utilisateur tape dans le champ
---
+++ exo_07_c.rb
@@ -0,0 +1,3 @@
+user_name = gets.chomp
+puts user_name
+#La commande puts permet de faire un retour à la ligne si la commande contient plusieurs éléments ce que la commande print ne fait pas.
